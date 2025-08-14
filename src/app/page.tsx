e48c1d0aa7ffaaa5629e8204958d0ec8072b84ac
Adem Enes Polat <ademenespolat@gmail.com>
2025-08-14 10:43:27 +0300

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,6 +23,7 @@ export default function Home() {
           <li className="tracking-[-.01em]">
             Save and see your changes instantly.
           </li>
+          <li className="tracking-[-.01em]">This is test line for vercel.</li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
